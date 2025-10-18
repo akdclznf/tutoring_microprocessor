e8d2639518505ba2961f6bfba8dafce5f4542ef6
akdclznf <2071166@hansung.ac.kr>
2025-10-19 00:55:13 +0900

    edit files

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # tutoring_microprocessor
 한성튜터링 마이크로프로세서실험 자료
+
+# 1주차
+led, buzzer, switch 동작
+
+# 2주차
+dc motor, rgb led 동작
+
+# 3주차
+타이머 & 카운터, 인터럽트 활용
--- header files/light.h
+++ header files/light.h
@@ -2,35 +2,28 @@ volatile uint16_t light = 0;
 
 // 조도 센서 초기화
 void adc_light(void) {
-	// ADC 초기화: AVCC를 기준 전압으로 설정, ADC1 채널 선택
-	ADMUX = (1 << REFS0);  // AVCC 기준 전압, ADC1 채널
+
+	// ADC 초기화
+	ADMUX = (1 << REFS0);  // AVCC 기준 전압
 	ADMUX &= ~(_BV(4));
 	ADMUX &= ~(_BV(3));
 	ADMUX &= ~(_BV(2));
 	ADMUX |= _BV(1);
 	ADMUX |= _BV(0);
+
 	// ADC 제어 레지스터 초기화
 	ADCSRA = 0;
-	ADCSRA |= _BV(7); // ADC 활성화 (ADEN = 1)
-	ADCSRA |= _BV(6); // ADC 변환 시작 (ADSC = 1)
+	ADCSRA |= _BV(7);  // ADC 활성화 (ADEN = 1)
+	ADCSRA |= _BV(6);  // ADC 변환 시작 (ADSC = 1)
 
 	// ADC prescaler 설정
 	ADCSRA |= _BV(2);  // prescaler = 128
 	ADCSRA |= _BV(1);
 	ADCSRA |= _BV(0);
 
-	// ADC 변환 시작
-	ADCSRA |= _BV(6);  // ADSC = 1; 변환 시작
-
 	// ADC 변환 완료 대기
 	while (ADCSRA & _BV(6));  // ADSC가 1이면 변환 중, 0이면 변환 완료
 
 	// 변환 완료 후 결과 읽기
 	light = ADCL | (ADCH << 8);  // 10비트 값을 읽기
-
-	printf("Light: %d\n", light);
-
-	// 1000ms 대기
-	_delay_ms(1000);
-	
 }
